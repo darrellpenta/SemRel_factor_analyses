ba627c28378c3a6d8f1f25e76a86400f9ef725dd
Darrell Jonathan Penta <d.penta@neu.edu>
2015-03-20 23:02:03 -0400

    80% complete figures

--- SemRel F2 ANOVAs.R
+++ SemRel F2 ANOVAs.R
@@ -2,6 +2,7 @@ rm(list = ls()) # clears environment
 library(languageR) # calls languageR library
 library(ggplot2)
 library(grid)
+library(extrafont)
 #
 # -----------------------------------PREPARE DATA FILE FOR ANALYSES---------------------------------
 #
@@ -221,7 +222,7 @@ ds <- data.frame(data = c(
   ))
 
 
-# PREPARE FIGURES-------------------------
+# PREPARE RESULTS TABLES-------------------------
 
 
 rel.int.pl   <- subset(d, related == "rel"   & integ  == "integ" & n2num == "plur")
@@ -316,48 +317,35 @@ mis.eff <-data.frame(
       sd(se.unr.int$error) / sqrt(length(se.unr.int$error)),
       sd(se.unr.uni$error) / sqrt(length(se.unr.uni$error)))
     )
-dff = data.frame(
-  Year = c("20","20","20","20","20","20","20","20"),
-  Trt = c("Irr","Irr","Irr","Irr","Dry","Dry","Dry","Dry"),
-  Variety = c("Variety2", "Variety2", "Variety1","Variety1","Variety2", "Variety2", "Variety1","Variety1"),
-  geno = c(1,2,1,2,1,2,1,2),
-  yield = c(6855,5955,6355,5055,3955,3455,3855,3355),
-  SE = c(612,713,713,414,615,616,617,518)
-)
 
 
+# ---PREPARE FIGURES
 
-g1 <- ggplot(data = dff, aes(x = interaction(Variety, Trt), y = yield, fill = factor(geno)))+
-  scale_fill_manual(values=c("red", "green", "blue","green","red", "green", "blue","green"))
-g1 <- g1+layer(geom="bar", stat="identity", position = position_dodge())
-g1 <- g1+geom_errorbar(aes(ymax = yield + SE, ymin = yield - SE), position = dodge, width = 0.2) 
-g1 <- g1+coord_cartesian(ylim = c(0, 7500))
-g1 <- g1+annotate("text", x = 1:4, y = - 400,
-           label = rep(c("Variety 1", "Variety 2"), 2)) +
-  annotate("text", c(1.5, 3.5), y = - 800, label = c("Irrigated", "Dry"))
-g1 <- g1  + theme_classic()
-g1 <- g1 + theme(plot.margin = unit(c(1, 1, 4, 1), "lines"),
-        axis.title.x = element_blank(),
-        axis.text.x = element_blank())
-g1
-# remove clipping of x axis labels
-g2 <- ggplot_gtable(ggplot_build(g1))
-g2$layout$clip[g2$layout$name == "panel"] <- "off"
-grid.draw(g2)
-g2
-
-g1 <- ggplot(data = mis.eff, aes(x = interaction(Integration, Relatedness), y = ErrRate, fill = interaction(Integration, Relatedness)))
-#scale_fill_manual(values=c("red", "green", "blue","green","red", "green", "blue","green"))
-g1 <- g1+layer(geom="bar", stat="identity", position = position_dodge())
-g1 <- g1+geom_errorbar(aes(ymax = ErrRate + SE, ymin = ErrRate - SE), position = dodge, width = 0.2) 
-g1 <- g1+coord_cartesian(ylim = c(0, 14))
-g1 <- g1+annotate("text", x = 1:4, y = - 400,
-                  label = rep(c("Integ", "Uninteg"), 2)) +
-  annotate("text", c(1.5, 3.5), y = - 800, label = c("Related", "Unrelated"))
-g1 <- g1  + theme_classic()
-g1 <- g1 + theme(plot.margin = unit(c(1, 1, 4, 1), "lines"),
+dodge  <- position_dodge(width = 0.9)
+g1     <- ggplot(data = mis.eff, aes(x = interaction(Integration, Relatedness), y = ErrRate, fill = interaction(Integration, Relatedness)))
+g1     <- g1 + layer(geom="bar", stat="identity", position = position_dodge())
+g1     <- g1 + scale_fill_manual(values=c("#990000", "#CC6666", "#000099", "#9999CC"))
+g1     <- g1 + guides(fill=FALSE)
+g1     <- g1 + geom_errorbar(aes(ymax = ErrRate + SE, ymin = ErrRate - SE), position = dodge, width = 0.2) 
+g1     <- g1 + coord_cartesian(ylim = c(0, 14))
+g1     <- g1 + scale_y_continuous(breaks=seq(0, 15, 2))
+g1     <- g1+annotate("text", x = 1:4, y = -1,
+                  label = rep(c("Integrated", "Unintegrated"), 2), size=6) +
+  annotate("text", c(1.5, 3.5), y = -2, label = c("Related", "Unrelated"), size=6)
+g1    <- g1  + theme_classic()
+g1    <- g1 +  theme(text = element_text(size=20))
+g1    <- g1 + theme(plot.margin = unit(c(1, 1, 4, 1), "lines"),
                  axis.title.x = element_blank(),
                  axis.text.x = element_blank())
+
+
+# sig.bar <- data.frame(x = c(1, 1, 4, 4), y = c(13, 14, 14, 13))
+# 
+# g1    <- g1 + geom_path(data = sig.bar, aes(x = x, y = y))
+
+#   annotate("text",x=1.5,y=27,label="p=0.012")+
+#   annotate("text",x=2.5,y=39,label="p<0.0001")+
+#   annotate("text",x=3.5,y=51,label="p<0.0001")
 g1
 # remove clipping of x axis labels
 g2 <- ggplot_gtable(ggplot_build(g1))
